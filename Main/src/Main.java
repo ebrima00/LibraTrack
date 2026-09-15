67a5bdcea6f174ed2ab94821467681f635c9c73a
ebrima00 <esaine29@gmail.com>
2026-09-15 16:32:59 +0000

    feat: model books with parallel arrays

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String[] bookNames = {"Things Fall Apart", "Clean Code"};
+        String[] authors = {"Chinua Achebe", "Robert Martin"};
+        int[] bookIds = {101, 102};
+        boolean[] available = {true, false};
+
+        for (int i = 0; i < bookNames.length; i++) {
+            System.out.println(
+                    bookIds[i] + " - "
+                            + bookNames[i] + " - "
+                            + authors[i] + " - "
+                            + available[i]
+            );
+        }
+
+        in.close();
     }
 }
